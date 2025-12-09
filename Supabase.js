b77db39a9479bfee17f4f45ceefdfc12c00a163b
hoobasurf <brice.paneela@icloud.com>
2025-12-09 20:40:47 +0100

    Delete Supabase.js

--- Supabase.js
+++
@@ -1,8 +0,0 @@
-// supabase.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-
-const SUPABASE_URL = 'https://iubbxvipgofxasatmvzg.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_GDoZmwIdoP28XOdrfYYVNw_E_HiCQB1';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-console.log('Supabase chargé', supabase);
