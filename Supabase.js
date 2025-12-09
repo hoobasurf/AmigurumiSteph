76cee3a05b848af910f1ee4495f11b6a67d75535
hoobasurf <brice.paneela@icloud.com>
2025-12-09 19:17:27 +0100

    Update Supabase.js

--- Supabase.js
+++ Supabase.js
@@ -1,9 +1,7 @@
-<script type="module">
-  import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-  // ⚡ Remplace par tes infos Supabase
-  const SUPABASE_URL = 'https://iubbxvipgofxasatmvzg.supabase.co';
-  const SUPABASE_ANON_KEY = 'TON_ANON_KEY_ICI';
+const SUPABASE_URL = 'https://iubbxvipgofxasatmvzg.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_GDoZmwIdoP28XOdrfYYVNw_E_HiCQB1';
 
-  export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-</script>
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+console.log('Supabase chargé', supabase);
