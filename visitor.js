80abc8664a83b037d8b5500d5bc17232a73dc5be
hoobasurf <brice.paneela@icloud.com>
2025-12-10 10:54:59 +0100

    Update visitor.js

--- visitor.js
+++ visitor.js
@@ -1,27 +1,13 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+const gallery = document.getElementById("gallery");
 
-const SUPABASE_URL = 'https://iubbxvipgofxasatmvzg.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1YmJ4dmlwZ29meGFzYXRtdnpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyODg1NzgsImV4cCI6MjA4MDg2NDU3OH0.cg6AbfQ-av-nyEtAdWetdRjHSKaYJUw2QY2kgCFgkVs';
+const saved = JSON.parse(localStorage.getItem("creations") || "[]");
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
-const list = document.getElementById('visitor-list');
-
-function addToList(item) {
-  const div = document.createElement('div');
-  div.className = 'visitor-item';
-  div.innerHTML = `<p>${item.name}</p><img src="${item.image_url}" class="mini-img">`;
-  list.prepend(div);
-}
-
-// Charger les créations existantes
-async function loadCreations() {
-  const { data, error } = await supabase
-    .from('creations')
-    .select('*')
-    .order('created_at', { ascending: false });
-  if (error) return console.error(error);
-  list.innerHTML = '';
-  data.forEach(addToList);
-}
-loadCreations();
+saved.forEach(item => {
+  const div = document.createElement("div");
+  div.className = "item";
+  div.innerHTML = `
+    <p>${item.name}</p>
+    <img src="${item.imgUrl}">
+  `;
+  gallery.appendChild(div);
+});
