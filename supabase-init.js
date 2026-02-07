bdf5ce2c88c3c265aa992b9d0c0642925b7cf72f
hoobasurf <brice.paneela@icloud.com>
2026-02-07 17:20:46 +0100

    Update supabase-init.js

--- supabase-init.js
+++ supabase-init.js
@@ -1,12 +1,10 @@
 // supabase-init.js — METTEZ À JOUR CES LIGNES
 
-// ❌ ANCIENNES (à supprimer)
-const SUPABASE_URL = "https://iubbxvipgofxasatmvzg.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_GDoZmwIdoP28XOdrfYYVNw_E_HiCQB1";
 
 // ✅ NOUVELLES (à mettre)
-const SUPABASE_URL = "https://jyhbamdcxmpveujjqjla.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp5aGJhbWRjeG1wdmV1ampxamxhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwMjYzOTMsImV4cCI6MjA4MjYwMjM5M30.PIOMZxprCdkFwFj_sUC0ZDNSIP5OcGmxkvYN7D3JmGY";
+const SUPABASE_URL = "https://lgduavepbznnzjfxlsnx.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxnZHVhdmVwYnpubnpqZnhsc254Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA0NzgyODcsImV4cCI6MjA4NjA1NDI4N30.6yAmtLvG0t7ne3sV2DXMbsCfcK8JVAi5NujZsQUJ_Tk";
+
 
 // Gardez le reste identique
 try {
