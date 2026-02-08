7d93810c1ee17b520e7d567f731f6534a7c5802c
hoobasurf <brice.paneela@icloud.com>
2026-02-08 10:42:16 +0100

    Update supabase-init.js

--- supabase-init.js
+++ supabase-init.js
@@ -6,6 +6,7 @@ const SUPABASE_URL = "https://lgduavepbznnzjfxlsnx.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxnZHVhdmVwYnpubnpqZnhsc254Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA0NzgyODcsImV4cCI6MjA4NjA1NDI4N30.6yAmtLvG0t7ne3sV2DXMbsCfcK8JVAi5NujZsQUJ_Tk";
 
 
+
 // Gardez le reste identique
 try {
   window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
